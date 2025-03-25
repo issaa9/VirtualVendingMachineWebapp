312085c0b16569d1544210aead89ebdece54ae50
issaa <issaaboobaker@hotmail.com>
2025-03-25 05:24:12 +0000

    New commit: added in a table of products in the admin dashboard. The table shows the product ids, names, categories, current stock and also contains an actions column with some buttons in and also an auto-update column with a basic checkbox. Modified admin controller method to use the product service method getAllProducts which retrieves a list of all products from the database. This is passed in to the page as a model attribute and used to populate the table. Created new external stylesheet admindashboard_styles.css which contains basic styles for the admin page and the table. Overall created basic layout and view of the page with basic styling, next just need to add the functionality and improve styling.

--- src/main/java/com/example/finalyearproject/controller/AdminController.java
+++ src/main/java/com/example/finalyearproject/controller/AdminController.java
@@ -1,16 +1,25 @@
 package com.example.finalyearproject.controller;
 
+import com.example.finalyearproject.model.Product;
+import com.example.finalyearproject.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 //controller for the admin dashboard and all admin controls
 @Controller
 public class AdminController {
 
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/admin/dashboard") //create endpoint for admin dashboard
-    public String showAdminDashboard() {
-
+    public String showAdminDashboard(Model model) {
+        List<Product> products = productService.getAllProducts(); //retrieve all products
+        model.addAttribute("products", products);  //add in products as model attribute
         return "admindashboard"; //render the admin dashboard page
     }
 }
